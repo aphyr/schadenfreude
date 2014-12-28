740c81d766b6a3d72445e935ecaf816f0f4d2d2b
Aphyr <aphyr@aphyr.com>
2014-12-27 21:23:05 -0800

    Move clojure dep to dev profile

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main schadenfreude.demo
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clojure-tools "1.1.2"]
-                 [interval-metrics "0.0.2"]
+  :dependencies [[clojure-tools "1.1.2"]
+                 [interval-metrics "1.0.0"]
                  [incanter/incanter-core "1.5.4"]
-                 [incanter/incanter-charts "1.5.4"]])
+                 [incanter/incanter-charts "1.5.4"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
